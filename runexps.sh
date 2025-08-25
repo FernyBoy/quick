205a123a653b6eb84811908fad4c72327423e71c
Rafael Morales-Gamboa <r.moralesgamboa@gmail.com>
2025-08-25 00:18:52 -0600

    Updates -e parameters in runexps.sh

--- runexps.sh
+++ runexps.sh
@@ -4,6 +4,6 @@ runpath=runs
 echo "Storing results in $runpath"
 python eam.py -n --runpath="$runpath" && \
     python eam.py -f --runpath="$runpath" && \
-    python eam.py -e 1 --runpath="$runpath" && \
+    python eam.py -e 1 4 --runpath="$runpath" && \
     python eam.py -r --runpath="$runpath" && \
     python eam.py -d --runpath="$runpath"
