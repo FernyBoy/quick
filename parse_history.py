0c643e9864bc366a6938a7ae65d2c87cee7e841b
Rafael Morales Gamboa <rmorales@suv.udg.mx>
2025-09-16 20:44:36 -0600

    Updates parse_history.py to the new settings

--- parse_history.py
+++ parse_history.py
@@ -1,8 +1,9 @@
 import json
 
-domain_sizes = [32, 64, 128, 256, 512]
+nums_rows = [2, 4, 8, 16, 24, 32]
 class_metric = 'accuracy'
 autor_metric = 'decoder_root_mean_squared_error'
+
 
 def print_keys(data):
     print('Keys: [ ', end='')
@@ -10,21 +11,22 @@ def print_keys(data):
         print(f'{k}, ', end='')
     print(']')
 
-if __name__ == "__main__":
-    prefix = 'runs-'
+
+if __name__ == '__main__':
+    prefix = 'runs_'
     suffix = '/model-classifier.json'
-    for domain in domain_sizes:
-        filename = f'{prefix}{domain}{suffix}'
+    for rows in nums_rows:
+        filename = f'{prefix}{rows}{suffix}'
         # Opening JSON file
         with open(filename, 'r') as f:
             data = json.load(f)
             history = data['history']
-            # In every three, the first element is the trace of the training, 
+            # In every three, the first element is the trace of the training,
             # and it is ignored. The second and third elements contain
             # the metric and loss for the classifier and autoencoder,
             # respectively
             print(f'History lenght: {len(history)}')
             for i in range(0, len(history), 3):
-                class_value = history[i+1][class_metric]
-                autor_value = history[i+2][autor_metric]
-                print(f'{domain},{class_value},{autor_value}')
+                class_value = history[i + 1][class_metric]
+                autor_value = history[i + 2][autor_metric]
+                print(f'{rows},{class_value},{autor_value}')
