38c81fee266f228e16e6be241f1809351f3a7555
Rafael Morales-Gamboa <r.moralesgamboa@gmail.com>
2025-12-05 18:13:22 -0600

    Removes memory size of 1

--- constants.py
+++ constants.py
@@ -162,7 +162,7 @@ response_behaviours = {
     incorrect_mis_response_idx: 'Incorrect Mis Response',
 }
 
-memory_sizes = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
+memory_sizes = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
 memory_fills = [1.0, 2.0, 4.0, 8.0, 16.0, 32.0, 64.0, 100.0]
 n_best_memory_sizes = 2
 
